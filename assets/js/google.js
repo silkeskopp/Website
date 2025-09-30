aa75c7bb5a135fbb1a96313a88c1d32eabc2e11b
eskopp <info@erik-skopp.de>
2025-09-30 19:14:28 +0200

    Refactor Google Analytics script to pure JS
    Replaces inline <script> tags with a self-contained JavaScript function that dynamically injects the Google Analytics script and initializes gtag. This improves modularity and allows the script to be included as a standalone JS file.

--- assets/js/google.js
+++ assets/js/google.js
@@ -1,8 +1,15 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-71GVZE7JW3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+(function() {
+  // Create the async script tag for gtag
+  var gtagScript = document.createElement("script");
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-71GVZE7JW3";
+  gtagScript.async = true;
+  document.head.appendChild(gtagScript);
 
-  gtag('config', 'G-71GVZE7JW3');
-</script>
+  // Initialize dataLayer and gtag
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag; // expose gtag globally
+
+  gtag("js", new Date());
+  gtag("config", "G-71GVZE7JW3");
+})();
