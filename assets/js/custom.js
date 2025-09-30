3ea6a77f4f36bef0e86784192b5e3d26eaf3012d
eskopp <info@erik-skopp.de>
2025-09-30 20:46:34 +0200

    Move Google Analytics code to custom.js
    Transferred Google Analytics initialization from google.js to custom.js and removed the now redundant google.js file. This change consolidates custom JavaScript logic into a single file.

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+// Create the async script tag for gtag
+var gtagScript = document.createElement("script");
+gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-71GVZE7JW3";
+gtagScript.async = true;
+document.head.appendChild(gtagScript);
+
+// Initialize dataLayer and gtag
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+window.gtag = gtag; // expose gtag globally
+
+gtag("js", new Date());
+gtag("config", "G-71GVZE7JW3");
+
+
+console.log("Hallo");
